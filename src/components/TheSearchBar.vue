a9b79f71f97c9422ab538ad48c7f569db86a82a2
spierre95 <stephen.pierre20@gmail.com>
2020-09-02 18:55:45 -0400

    added dropdown component to searchbar component

--- src/components/TheSearchBar.vue
+++ src/components/TheSearchBar.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="search-bar">
-    <b-form-input v-model="input" placeholder="Enter your name"></b-form-input>
+  <div class="row search-bar">
+    <div class="col-4">
+      <VDropDown
+        :selectedOption="searchBy"
+        :options="searchByOptions"
+        @option-selected="updateSearchBy"
+      />
+    </div>
+    <div class="col-8">
+      <b-form-input
+        v-model="input"
+        :placeholder="placeholderText"
+      ></b-form-input>
+    </div>
   </div>
 </template>
 
 <script>
+import VDropDown from "./VDropDown";
 export default {
   name: "TheSearchBar",
+  components: {
+    VDropDown,
+  },
+  methods: {
+    updateSearchBy: function(value) {
+      console.log(value);
+      this.searchBy = value;
+    },
+  },
+  computed: {
+      placeholderText: function(){
+          return `search cocktail by ${this.searchBy}`
+      }
+  },
   data() {
     return {
       input: "",
+      // default
+      searchBy: "name",
+      searchByOptions: ["name", "ingredient"],
     };
   },
 };
---
+++ src/components/VDropDown.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-dropdown :text="selectedOption">
+    <b-dropdown-item
+      v-for="option in options"
+      :key="option"
+      @click="handleSelectedOption(option)"
+      >{{ option }}</b-dropdown-item
+    >
+  </b-dropdown>
+</template>
+
+<script>
+export default {
+  name: "VDropDown",
+  props: {
+    selectedOption: {
+      type: String,
+      default: "",
+    },
+    options: {
+      type: Array,
+      deafult: function() {
+        return [];
+      },
+    },
+  },
+  methods:{
+      handleSelectedOption: function(option) {
+          console.log(option)
+          this.$emit('option-selected', option)
+      }
+  }
+};
+</script>
