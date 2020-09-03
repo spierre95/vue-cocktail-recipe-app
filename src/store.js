5557e9368c9cf81f20e097fc6b7331933f02c057
spierre95 <stephen.pierre20@gmail.com>
2020-09-02 20:24:46 -0400

    update store with search results

--- src/store.js
+++ src/store.js
@@ -1,33 +1,61 @@
 import Vue from "vue";
-import axios from 'axios'
+import axios from "axios";
 import Vuex from "vuex";
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     fetching: false,
+    drinks: []
   },
-  mutations: {},
+  mutations: {
+    updateDrinkSearchResults(state, payload){
+        Vue.set(state, 'drinks', payload)
+    }
+  },
   actions: {
-    fetchCocktailRecipesByName({}, input) {
-      axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php", {
+    fetchCocktailRecipesByName({ commit }, input) {
+      axios
+        .get("https://www.thecocktaildb.com/api/json/v1/1/search.php", {
           params: {
-            "s": input,
+            s: input,
           },
         })
         .then((res) => {
-          console.log(res);
+          const data = res.data.drinks.map((drink) => {
+            const {
+              dateModified,
+              idDrink,
+              strAlcoholic,
+              strCategory,
+              strCreativeCommonsConfirmed,
+              strDrink,
+              strDrinkThumb,
+            } = drink;
+            return {
+              dateModified,
+              idDrink,
+              strAlcoholic,
+              strCategory,
+              strCreativeCommonsConfirmed,
+              strDrink,
+              strDrinkThumb,
+            };
+          });
+          commit('updateDrinkSearchResults', data)
         })
-        .catch((res) => {});
+        .catch((error) => console.error(error));
     },
-    fetchCocktailRecipesByIngredient({}, input) {
-      axios.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php", {
+    fetchCocktailRecipesByIngredient({ commit }, input) {
+      axios
+        .get("https://www.thecocktaildb.com/api/json/v1/1/filter.php", {
           params: {
-            "i": input,
+            i: input,
           },
         })
         .then((res) => {
-          console.log(res);
+          const data = res.data.drinks;
+          commit('updateDrinkSearchResults', data)
         })
         .catch((res) => {});
     },
